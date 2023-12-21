ac43aae942f25d8014bbd9de59d61ec1bf1569f3
DESKTOP-JI02I8J\Yourdworld <vcyourdworld@gmail.com>
2023-12-21 19:14:37 +0530

    rfe

--- public/script.js
+++ public/script.js
@@ -22,7 +22,7 @@ showChat.addEventListener("click", () => {
 const user = prompt("Enter your name");
 
 var peer = new Peer({
-  host: '127.0.0.1',
+  host: '52.41.36.82',
   port: 3030,
   path: '/peerjs',
   config: {
